3db7f0d4824c89b5189fe7e605bd9faafc65f95f
Edwardjsmith <2smite17@solent.ac.uk>
2018-10-03 14:08:33 +0100

    Eds Scripts

---
+++ TheJokeYou/Assets/Scenes/jokeCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jokeCollision : MonoBehaviour
+{
+    Joke joke;
+    Score score;
+
+    private void Start()
+    {
+        joke = GetComponent<Joke>();
+        score = GameObject.Find("player").GetComponent<Score>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "trigger")
+        {
+            score.resetCombo();
+        }
+
+        joke.ResetPosition();
+    }
+}
---
+++ TheJokeYou/Assets/Scenes/playerCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+    Score score;
+
+    private void Start()
+    {
+        score = GetComponent<Score>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Joke")
+        {
+            score.ScoreIncrement();
+        }
+    }
+}
