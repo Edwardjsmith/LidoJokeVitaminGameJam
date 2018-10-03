c13bfdc242cac483c7fe05e69bd207e0429b052a
Edwardjsmith <2smite17@solent.ac.uk>
2018-10-03 15:54:55 +0100

    Changes

---
+++ TheJokeYou/Assets/stayRelativeToPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stayRelativeToPlayer : MonoBehaviour {
+
+    Vector3 playerPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerPos = GameObject.Find("Player").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		MeshRenderer mesh = GetComponent<MeshRenderer>();
+        Material material = mesh.material;
+        Vector2 materialOffset = material.mainTextureOffset;
+
+        transform.position = playerPos + new Vector3(0, 4.39f, 0);
+
+        materialOffset.x = transform.position.x / transform.localScale.x;
+        materialOffset.y = transform.position.y / transform.localScale.y;
+        material.mainTextureOffset = materialOffset;
+	}
+}
