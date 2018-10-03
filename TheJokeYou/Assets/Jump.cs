056737543d4cdfba2d1c7329267849c160d69810
Jack Porter <AFKmuch@users.noreply.github.com>
2018-10-03 14:52:13 +0100

    have some shit

--- TheJokeYou/Assets/Jump.cs
+++ TheJokeYou/Assets/Jump.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jump : MonoBehaviour {
-
+    public enum State
+        {
+        Idle,
+        Crouch,
+        Jump
+    }
     public float JumpPower;
-
+    public State state;
     public Rigidbody2D rigidbody;
+    public AnimationClip jump;
+    public AnimationClip crouch;
+    public AnimationClip idle;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +23,10 @@ public class Jump : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        if(Physics2D.Raycast(transform.position, Vector2.down, 1))
+        Debug.DrawRay(transform.position, Vector2.down, Color.red, 0.5f);
+        if(Physics2D.Raycast(transform.position, Vector2.down, GetComponent<SpriteRenderer>().sprite.rect.height /2))
         {
+            state = State.Idle;
             if (Input.GetButtonDown("Jump"))
             {
                 JumpPower = 0;
@@ -25,14 +34,24 @@ public class Jump : MonoBehaviour {
 
             if (Input.GetButton("Jump"))
             {
-                JumpPower += 1 * Time.deltaTime;
-                Mathf.Clamp(JumpPower, 0, 15);
+                JumpPower += 5 * Time.deltaTime;
+                if(JumpPower > 13)
+                {
+                    JumpPower = 13;
+                }
+                state = State.Crouch;
             }
-
             if (Input.GetButtonUp("Jump"))
             {
                 rigidbody.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
             }
         }
+        else
+        {
+            state = State.Jump;
+        }
+
+        GetComponent<Animator>().SetInteger("State", (int) state);
+        print(GetComponent<SpriteRenderer>().sprite.rect.height / 2);
 	}
 }
