47e172c409a501f838e4f9b50eeda4e7fe6e74e3
Nik <sticknik@ymail.com>
2018-10-03 15:21:35 +0100

    UI
    Game and menu UI  + script + prefab

---
+++ TheJokeYou/Assets/Scoreboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour {
+
+    public ScrollRect ScrollView;
+    public GameObject ScoreContent;
+    public GameObject HighScorePrefab;
+
+    private int highscore;
+
+    void Start ()
+    {
+        for (int i = 0; i < ScoreList.length; i++)
+        {
+            highscore = ScoreList[count];
+
+            GameObject HighScoreObj = Instantiate(HighScorePrefab);
+            HighScoreObj.transform.SetParent(ScoreContent.transform, false);
+            HighScoreObj.transform.Find("Score").gameObject.GetComponent<Text>().text = highscore.ToString();
+        }
+    }
+}
