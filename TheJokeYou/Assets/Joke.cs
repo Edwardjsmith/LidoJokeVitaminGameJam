e53607b58182574edebdd7eb6e7f4152b35a1f4e
Widdiful <Widdiful@users.noreply.github.com>
2018-10-03 14:02:35 +0100

    Started joke script

---
+++ TheJokeYou/Assets/Joke.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joke : MonoBehaviour {
+
+    public float startX;
+    public float maxY;
+    public float minY;
+    public float speed;
+    public float timeToNextJoke;
+
+	public void ResetPosition()
+    {
+
+    }
+}
